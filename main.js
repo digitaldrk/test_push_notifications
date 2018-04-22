dc0d50e0448768058f0de1daad399c40c4d21bb6
Derek Dyer <derek@digitalderek.com>
2018-04-22 12:57:55 -0400

    Initial commit

---
+++ main.js
@@ -0,0 +1,65 @@
+function askPermission() {
+  return new Promise(function(resolve, reject) {
+    const permissionResult = Notification.requestPermission(function(result) {
+      resolve(result);
+      if (result === 'granted') {
+        subscribeUser();
+      }
+    });
+
+    if (permissionResult) {
+      permissionResult.then(resolve, reject);
+    }
+  }).then(function(permissionResult) {
+    if (permissionResult !== 'granted') {
+      throw new Error('We weren\'t granted permission.');
+    }
+  });
+}
+
+function subscribeUser() {
+  const applicationServerKey = urlBase64ToUint8Array('BHdr03Mr4ZU7A2C0kfLCtFQ_Dsl3vFVZL251jtAwVJC_bwKm9p8XIPD38x0BR7mgKjVACAhuCi6LTWWa1gMfOqM=');
+  const options = {
+    userVisibleOnly: true,
+    applicationServerKey: applicationServerKey
+  }
+
+  return swRegistration.pushManager.subscribe(options).then(function(subscription) {
+    sendSubscriptionToBackEnd(subscription)
+  }).catch(function(err) {
+    console.log('Failed to subscribe the user: ', err);
+  });
+}
+
+function urlBase64ToUint8Array(base64String) {
+  const padding = '='.repeat((4 - base64String.length % 4) % 4);
+  const base64 = (base64String + padding).replace(/\-/g, '+').replace(/_/g, '/');
+
+  const rawData = window.atob(base64);
+  const outputArray = new Uint8Array(rawData.length);
+
+  for (let i = 0; i < rawData.length; ++i) {
+    outputArray[i] = rawData.charCodeAt(i);
+  }
+  return outputArray;
+}
+
+function sendSubscriptionToBackEnd(subscription) {
+  return fetch('https://arcane-stream-87798.herokuapp.com/subscriptions', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(subscription)
+  }).then(function(response) {
+    if (!response.ok) {
+      throw new Error('Bad status code from server.');
+    }
+
+    return response.json();
+  }).then(function(responseData) {
+    if (!(responseData.data && responseData.data.success)) {
+      throw new Error('Bad response from server.');
+    }
+  });
+}
---
+++ service-worker.js
@@ -0,0 +1,13 @@
+self.addEventListener("push", (event) => {
+  let messageContent = {};
+  if(event.data) {
+    messageContent = event.data.json();
+  }
+
+  let title = messageContent.title;
+  let body = messageContent.body;
+  let tag = "FooBar Tag";
+  let icon = 'https://vignette.wikia.nocookie.net/thehungergamesrp/images/0/0d/Troll_tribute_icon.png';
+
+  event.waitUntil(self.registration.showNotification(title, {body, icon, tag}))
+});
