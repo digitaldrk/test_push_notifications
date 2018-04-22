664b465353a65a3310a56b64d79a9efc8ef22faf
Derek Dyer <derek@digitalderek.com>
2018-04-22 13:18:33 -0400

    Revert so it works, no more fancy stuff

--- service-worker.js
+++ service-worker.js
@@ -1,13 +1,10 @@
 self.addEventListener("push", (event) => {
-  let messageContent = {};
-  if(event.data) {
-    messageContent = JSON.parse(event.data);
-  }
+    let title = (event.data && event.data.text()) || "Yay a message";
+    let body = "We have received a push message";
+    let tag = "push-simple-demo-notification-tag";
+    let icon = '/assets/my-logo-120x120.png';
 
-  let title = messageContent.title;
-  let body = messageContent.body;
-  let tag = "FooBar Tag";
-  let icon = 'https://vignette.wikia.nocookie.net/thehungergamesrp/images/0/0d/Troll_tribute_icon.png';
-
-  event.waitUntil(self.registration.showNotification(title, {body, icon, tag}))
-});
+    event.waitUntil(
+      self.registration.showNotification(title, { body, icon, tag })
+    )
+  });
